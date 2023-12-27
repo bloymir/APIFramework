07e906959cec60c2d5ea42b1b3c4c9e9b2aff3c4
Nelson Tapia <ntapia@bancoconsorcio.cl>
2023-12-27 16:54:54 -0300

    se deja funcionado

--- src/test/java/Steps/APISteps.java
+++ src/test/java/Steps/APISteps.java
@@ -1,15 +1,36 @@
 package Steps;
 
 import io.cucumber.java.en.*;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
 
 public class APISteps {
-    @Given("Envio un request tipo Get a un endpoint")
-    public void sendGetRequest(){
-        System.out.println("Prueba Step 1");
+
+    private static RequestSpecification request;
+    private static Response response;
+    private ValidatableResponse json;
+
+    @Given("^Envio un request tipo Get a un endpoint (.+) URI$")
+    public void sendGetRequest(String URI){
+        request = given()
+                .baseUri(URI)
+                .contentType(ContentType.JSON);
     }
 
-    @Then("Obtengo una lista de 10 usuarios")
+    @Then("^Obtengo una lista de 10 usuarios$")
     public void getResponse(){
         System.out.println("Prueba Step 2");
+    }
+
+    @Then("Obtengo un status code {int}")
+    public void getStatusCode(int expectedStatusCode){
+        response = request
+                .when()
+                .get("/users/bloymir/repos");
+
+        json = response.then().statusCode(expectedStatusCode);
     }
 }
--- src/test/resources/features/APITest.feature
+++ src/test/resources/features/APITest.feature
@@ -1,4 +1,4 @@
-Feature: Ejemplo de request GET
+Feature: Ejemplo de request GET de Github
   Scenario: Prueba Get a un endpoint
-    Given Envio un request tipo Get a un endpoint
-    Then Obtengo una lista de 10 usuarios
+    Given Envio un request tipo Get a un endpoint https://api.github.com URI
+    Then Obtengo un status code 200
